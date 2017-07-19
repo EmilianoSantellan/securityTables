1198d866ca680e099c88e9fcbc2d0a266749fc83
Emiliano Santella <esantellan>
2017-07-18 22:23:09 -0300

    service api generic

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Headers, Http, Response, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class ApiService {
+
+  constructor(
+    private http: Http
+  ) {}
+
+   private setHeaders(): Headers {
+    const headersConfig = {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    };
+
+    return new Headers(headersConfig);
+  }
+
+  private formatErrors(error: any) {
+     return Observable.throw(error.json());
+  }
+
+  get(path: string, params: URLSearchParams = new URLSearchParams()): Observable<any> {
+    return this.http.get(`${environment.api_url}${path}`, { headers: this.setHeaders(), search: params, withCredentials: true })
+    .catch(this.formatErrors)
+    .map((res: Response) => res.json());
+  }
+
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http.put(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body),
+      { headers: this.setHeaders(), withCredentials: true }
+    )
+    .catch(this.formatErrors)
+    .map((res: Response) => res.json());
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body),
+      { headers: this.setHeaders(), withCredentials: true }
+    )
+    .catch(this.formatErrors)
+    .map((res: Response) => res.json());
+  }
+
+  delete(path): Observable<any> {
+    return this.http.delete(
+      `${environment.api_url}${path}`,
+      { headers: this.setHeaders(), withCredentials: true }
+    )
+    .catch(this.formatErrors)
+    .map((res: Response) => res.json());
+  }
+
+}
